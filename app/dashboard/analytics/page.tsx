3453dda904ccd1232f88529632068a775986144d
Tejas Yogesh <yogeshtejaslalitha@gmail.com>
2025-02-18 18:46:21 +0530

    ChangesinPageofAboutandDashboard

---
+++ app/dashboard/analytics/page.tsx
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+  return (
+    <div>This is Analytics</div>
+  )
+}
+
+export default page
---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+  return (
+    <div>This is user page</div>
+  )
+}
+
+export default page
