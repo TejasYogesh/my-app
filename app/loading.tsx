0d60c52dd74fb62af047b3ad456195d42bca0bc4
Tejas Yogesh <yogeshtejaslalitha@gmail.com>
2025-02-20 22:12:46 +0530

    loading.tsxfileAdded

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const loading = () => {
-  return (
-    <div>===============================Loading================================</div>
-  )
+    return (
+        <div className='text-4xl text-center m-10 flex justify-center items-center h-screen'>
+            <p className='text-center'>Please Wait</p>
+        </div>
+    )
 }
 
 export default loading
